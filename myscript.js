519595c26321205cb558cdc0cada49bf8fd12c5c
bency <bency@pixnet.tw>
2014-12-21 00:07:13 +0800

    initial commit

---
+++ myscript.js
@@ -0,0 +1,49 @@
+not_notify = true;
+notification = null
+function notifyMe(text) {
+    if (!Notification) {
+        alert('Please us a modern version of Chrome, Firefox, Opera or Firefox.');
+        return;
+    }
+    
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    
+    notification = new Notification('LiveAny', {
+        icon: 'http://www.liveany.com/favicon.ico',
+        body: text,
+    });
+    
+    if (notification) {
+        not_notify = false;
+    }
+    
+    notification.onclick = function () {
+        not_notify = true;
+        if (notification.body.match(/陌生人離開～～/)) {
+            $('title').text('陌生人已離開');
+        }
+        window.focus();
+        $('#inputText').focus();
+    }
+}
+$(document).ready(function() {
+    $("#display_area").bind("DOMSubtreeModified", function() {
+        message = $(this).children().last().clone().children().remove().end().text();
+        if (message.match(/^陌生人/) && !document.hasFocus()) {
+            if (!not_notify) {
+                notification.close();
+                notification = null;
+            }
+            if (message.match(/陌生人離開～～/)) {
+                $('title').text('陌生人已離開');
+            }
+            notifyMe(message);
+        }
+    });
+    $(window).focus(function() {
+        if (notification) {
+            notification.close();
+        }
+    })
+});
